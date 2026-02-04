8fc86ca0bdecb4591901b49bb95386851571bc21
rninecraftpidoor-hub <rninecraftpidoor@gmail.com>
2026-02-04 21:59:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-function openAuthors() {
-    document.getElementById("menu").classList.remove("active");
-    document.getElementById("authors").classList.add("active");
+function showAuthors() {
+  document.getElementById("authors").style.display = "flex";
 }
 
-function backToMenu() {
-    document.getElementById("authors").classList.remove("active");
-    document.getElementById("menu").classList.add("active");
+function hideAuthors() {
+  document.getElementById("authors").style.display = "none";
 }
